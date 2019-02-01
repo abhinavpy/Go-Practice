a0c864363dce0f4077164797a226eba798b12c2a
abhinavpy <py.abhinav2@gmail.com>
2019-02-02 00:18:33 +0530

    Added new files

--- WritingWebApplications/wiki.go
+++ WritingWebApplications/wiki.go
@@ -2,34 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 
-type Page struct {
-	Title string
-	body []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, body: body}, nil
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, there, I love %s!", r.URL.Path[1:])
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s<div>", p.Title, p.body)
+	//Note the use of _ to ignore the return value from loadPage
+	//First, this function extracts the page title from r.URL.Path, the
+	//path component of the request URL. The Path is re-sliced with
+	//[len("/view/"):] to drop the leading "/view/" component of the request pat.
+	//This is because the path will invariable begin with "/view/", which is
+	//not part of the page's title.
+	//The function then loads the page data, formats the page with a string of simple HTML, and writes it to w, the http.ResponseWriter.
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,20 +30,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.body)
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w,p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
-
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
-
-
